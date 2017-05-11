fb63b14120d866a873191921fd2c18a7a0bbe4ef
Universum Spiritus <universum.spiritus@gmail.com>
2017-05-11 14:23:44 +1000

    Added new mind maps for NextIsWhat, Neo4j, Hadoop Application Archi, Mining frequent patterns, Data Mining Concepts & Techniques Preface, Getting started with data mining with python

---
+++ notes/Data Mining Concepts and Techniques 3rd ed/Ch 06 Mining Frequent Patterns.mm
@@ -0,0 +1,229 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Ch 6" FOLDED="false" ID="ID_451566638" CREATED="1494364640542" MODIFIED="1494364775919" STYLE="oval">
+<font SIZE="14"/>
+<hook NAME="MapStyle" zoom="1.5">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="9" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="theme" POSITION="right" ID="ID_291338194" CREATED="1494364763922" MODIFIED="1494364832367">
+<edge COLOR="#ff0000"/>
+<node TEXT="    discovering knowledge like" FOLDED="true" ID="ID_1849661678" CREATED="1494364791649" MODIFIED="1494364806945">
+<node TEXT="data relationships" ID="ID_1982138423" CREATED="1494367812882" MODIFIED="1494367820705">
+<node TEXT="recurring relationships in data" ID="ID_1612977218" CREATED="1494368070472" MODIFIED="1494368102291"/>
+<node TEXT="associations" ID="ID_788096806" CREATED="1494364814958" MODIFIED="1494364817705"/>
+<node TEXT="correlations" ID="ID_126736506" CREATED="1494364818126" MODIFIED="1494364821025"/>
+</node>
+<node TEXT="how" ID="ID_456826643" CREATED="1494367989954" MODIFIED="1494367992077">
+<node TEXT="mining frequent patterns" ID="ID_442438701" CREATED="1494367996142" MODIFIED="1494368002592"/>
+</node>
+<node TEXT="operationalized as" ID="ID_21893455" CREATED="1494368243055" MODIFIED="1494368250378">
+<node TEXT="association rules" ID="ID_1838906464" CREATED="1494368262847" MODIFIED="1494368266255"/>
+</node>
+<node TEXT="solving business problems" FOLDED="true" ID="ID_885584805" CREATED="1494368337141" MODIFIED="1494368352099">
+<node TEXT="catalog design" ID="ID_83810012" CREATED="1494368353257" MODIFIED="1494368356950"/>
+<node TEXT="cross-marketing" ID="ID_669368639" CREATED="1494368359994" MODIFIED="1494368364723"/>
+<node TEXT="understanding customer shopping behavior" ID="ID_1093154497" CREATED="1494368366906" MODIFIED="1494368404512"/>
+<node TEXT="selective marketing" ID="ID_838391796" CREATED="1494368525494" MODIFIED="1494368530329"/>
+<node TEXT="&quot;shelf placement&quot;" ID="ID_1410145190" CREATED="1494368551311" MODIFIED="1494368579725"/>
+<node TEXT="reverse sale offers" ID="ID_184244854" CREATED="1494368694619" MODIFIED="1494368701836">
+<node TEXT="buy computer because printers are on a sale" ID="ID_1693352348" CREATED="1494368705273" MODIFIED="1494368720578"/>
+<node TEXT="buy costlier shaving gel because face wash is free" ID="ID_1564075922" CREATED="1494368720997" MODIFIED="1494369076991"/>
+</node>
+<node TEXT="store layouts" ID="ID_551997637" CREATED="1494370115818" MODIFIED="1494370119320">
+<node TEXT="associated products - near by" ID="ID_1821721610" CREATED="1494370120513" MODIFIED="1494370276891">
+<node TEXT="immediate sale" ID="ID_1196965286" CREATED="1494370288535" MODIFIED="1494370307749"/>
+</node>
+<node TEXT="associated products - far off" ID="ID_1475131723" CREATED="1494370135968" MODIFIED="1494370143937">
+<node TEXT="potential sale" ID="ID_718847002" CREATED="1494370280864" MODIFIED="1494370316302"/>
+</node>
+<node TEXT="subtle psychology" ID="ID_1485085819" CREATED="1494370165468" MODIFIED="1494370181259"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="frequent patterns" FOLDED="true" POSITION="right" ID="ID_574422999" CREATED="1494364859945" MODIFIED="1494368057418">
+<edge COLOR="#7c0000"/>
+<node TEXT="patterns that occur frequently in data sets" ID="ID_412149874" CREATED="1494364896231" MODIFIED="1494364920888"/>
+<node TEXT="pattern morphology" ID="ID_1069337618" CREATED="1494364921442" MODIFIED="1494367914004">
+<node TEXT="frequent itemsets" ID="ID_157686802" CREATED="1494364962207" MODIFIED="1494367720552">
+<node TEXT="which items occur together in transactions" ID="ID_1468504887" CREATED="1494365921823" MODIFIED="1494365949251">
+<node TEXT="(i1, i2, i3) .." ID="ID_1295058991" CREATED="1494367265214" MODIFIED="1494367305329"/>
+</node>
+</node>
+<node TEXT="frequent sequences / sub-sequences" ID="ID_1109597611" CREATED="1494364974048" MODIFIED="1494367779701">
+<node TEXT="sequential patterns occuring in historical transactions" ID="ID_1250870033" CREATED="1494366299137" MODIFIED="1494366594904">
+<node TEXT="(i1 -&gt; i2 -&gt; i3)..." ID="ID_1695066646" CREATED="1494367280527" MODIFIED="1494367301978"/>
+</node>
+</node>
+<node TEXT="frequent structures / sub-structures" ID="ID_1835865048" CREATED="1494364965285" MODIFIED="1494367785025">
+<node TEXT="forms" ID="ID_1243735236" CREATED="1494366647589" MODIFIED="1494366651149">
+<node TEXT="subtree" ID="ID_1419892930" CREATED="1494366652081" MODIFIED="1494366654925">
+<node TEXT=" (subtr1)..(subtr2)" ID="ID_331067286" CREATED="1494367311066" MODIFIED="1494367359530"/>
+</node>
+<node TEXT="sublattices" ID="ID_1842277767" CREATED="1494366655336" MODIFIED="1494366657823">
+<node TEXT="(sublat1) ..(sublat2)" ID="ID_873713238" CREATED="1494367363441" MODIFIED="1494367378234"/>
+</node>
+<node TEXT="subgraphs" ID="ID_1557766611" CREATED="1494366658200" MODIFIED="1494366662976">
+<node TEXT="(subgr1)..(subgr2)" ID="ID_98521012" CREATED="1494367401601" MODIFIED="1494367409147"/>
+</node>
+</node>
+</node>
+<node TEXT="frequent combinations" ID="ID_173172626" CREATED="1494367496491" MODIFIED="1494367792480">
+<node TEXT="(subtr1, i1)" ID="ID_1218800345" CREATED="1494367502168" MODIFIED="1494367517829"/>
+<node TEXT="(subtr2, subtr2)" ID="ID_11040231" CREATED="1494367518646" MODIFIED="1494367532464"/>
+<node TEXT="(subtr1, subgr1) -&gt; (subtr2, subgr2)" ID="ID_63002883" CREATED="1494367575818" MODIFIED="1494367615185"/>
+</node>
+<node TEXT="similar frequent patterns by dimensions" ID="ID_381758239" CREATED="1494376943379" MODIFIED="1494377219076">
+<node TEXT="time" ID="ID_1215109826" CREATED="1494376985826" MODIFIED="1494376986774">
+<node TEXT="e.g. (bread, milk) -----&gt; (wheat bread, soy milk)" ID="ID_736041401" CREATED="1494377002635" MODIFIED="1494377027813"/>
+</node>
+<node TEXT="geography" ID="ID_1660285200" CREATED="1494376987186" MODIFIED="1494377150927">
+<node TEXT="e.g. (israel: rye bread, goat milk),&#xa;(india: white bread, cow milk)" ID="ID_1752823902" CREATED="1494377152131" MODIFIED="1494377154229"/>
+</node>
+<node TEXT="growth" ID="ID_1704383679" CREATED="1494376992842" MODIFIED="1494376997231">
+<node TEXT="e.g. (10-15 yrs:" ID="ID_1065003819" CREATED="1494377157954" MODIFIED="1494377203690"/>
+</node>
+</node>
+</node>
+<node TEXT="association rules" ID="ID_664571114" CREATED="1494376705014" MODIFIED="1494376714021">
+<node TEXT="format" ID="ID_1537831647" CREATED="1494377670658" MODIFIED="1494377672929">
+<node TEXT="prodA --&gt; prod B" ID="ID_8948924" CREATED="1494377315176" MODIFIED="1494377324998"/>
+<node TEXT="implication" ID="ID_65517275" CREATED="1494380722703" MODIFIED="1494380729192"/>
+</node>
+<node TEXT="evaluation&#xa;interestingness" ID="ID_1138074476" CREATED="1494377705719" MODIFIED="1494377729306">
+<node TEXT="support" ID="ID_1960392936" CREATED="1494376714852" MODIFIED="1494376716370">
+<node TEXT="usefulness of the rule" ID="ID_1099796069" CREATED="1494376751211" MODIFIED="1494376759087">
+<node TEXT="subset of transactions for which the rule is true&#xa;/ total number of transactions" ID="ID_1634831598" CREATED="1494377929272" MODIFIED="1494377978242"/>
+</node>
+<node TEXT="min support threshold" ID="ID_1601780035" CREATED="1494378979242" MODIFIED="1494378990280"/>
+</node>
+<node TEXT="confidence" ID="ID_924078653" CREATED="1494376716661" MODIFIED="1494376719353">
+<node TEXT="certainty of the rule" ID="ID_1290267748" CREATED="1494376765797" MODIFIED="1494376772782">
+<node TEXT="subset of transactions for which the rule is true&#xa;/ number of transactions in which prodA is bought" ID="ID_143115042" CREATED="1494378012218" MODIFIED="1494378064142"/>
+<node TEXT="assumptions" ID="ID_1297001367" CREATED="1494378087474" MODIFIED="1494378117257">
+<node TEXT="prodA is bought more often than prodB" ID="ID_1279874097" CREATED="1494378118913" MODIFIED="1494378120998"/>
+<node TEXT="prodA is the trigger" ID="ID_496898101" CREATED="1494378121585" MODIFIED="1494378129058"/>
+<node TEXT="prodB complements prodA" ID="ID_1311939229" CREATED="1494378146863" MODIFIED="1494378159443"/>
+</node>
+</node>
+<node TEXT="min support threshold" ID="ID_976078152" CREATED="1494378996061" MODIFIED="1494379001740"/>
+</node>
+<node TEXT="100 transactions" ID="ID_763817352" CREATED="1494377742079" MODIFIED="1494377820583">
+<node TEXT="50 mobiles" ID="ID_1176988813" CREATED="1494377821742" MODIFIED="1494377825440">
+<node TEXT="20 covers" ID="ID_1491689443" CREATED="1494377841060" MODIFIED="1494377857817"/>
+<node TEXT="30 screen-guards" ID="ID_399662713" CREATED="1494377858208" MODIFIED="1494377863761"/>
+</node>
+<node TEXT="50 laptops" ID="ID_559371056" CREATED="1494377826110" MODIFIED="1494377828190">
+<node TEXT="20 mice" ID="ID_95218" CREATED="1494377829339" MODIFIED="1494377835094"/>
+<node TEXT="30 bags" ID="ID_788286167" CREATED="1494377835578" MODIFIED="1494377839211"/>
+</node>
+<node TEXT="association rule" ID="ID_722089382" CREATED="1494377871961" MODIFIED="1494377881060">
+<node TEXT="(mobile, cover)" ID="ID_183360579" CREATED="1494377881994" MODIFIED="1494377891277">
+<node TEXT="support: 20/100" ID="ID_1556585704" CREATED="1494377892039" MODIFIED="1494377904789"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="market basket&#xa;analysis" FOLDED="true" POSITION="right" ID="ID_1268235806" CREATED="1494379096927" MODIFIED="1494379109235">
+<edge COLOR="#007c00"/>
+<node TEXT="representing real world" ID="ID_248242756" CREATED="1494380202260" MODIFIED="1494380223861">
+<node TEXT="entities" ID="ID_509522311" CREATED="1494380224390" MODIFIED="1494380235518">
+<node TEXT="store" ID="ID_1917761685" CREATED="1494380236324" MODIFIED="1494380237611"/>
+<node TEXT="item in the store" ID="ID_241092151" CREATED="1494380238196" MODIFIED="1494380274893"/>
+<node TEXT="customer" ID="ID_824376808" CREATED="1494380245867" MODIFIED="1494380254036">
+<node TEXT="trip" ID="ID_1614180426" CREATED="1494380254755" MODIFIED="1494380256688"/>
+<node TEXT="transaction" ID="ID_1745738496" CREATED="1494380257051" MODIFIED="1494380277841"/>
+</node>
+</node>
+<node TEXT="representation" ID="ID_779447796" CREATED="1494380289498" MODIFIED="1494380298376">
+<node TEXT="transaction&#xa;or basket" ID="ID_1362734903" CREATED="1494380299212" MODIFIED="1494380521258">
+<node TEXT="a vector of Boolean variables&#xa;of length equal to number of items in the store" ID="ID_913300489" CREATED="1494380357980" MODIFIED="1494380499697"/>
+<node TEXT="1 - purchase, 0 - no" ID="ID_10008491" CREATED="1494380536213" MODIFIED="1494380554404"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="frequent itemsets" POSITION="right" ID="ID_1978150202" CREATED="1494380617830" MODIFIED="1494380632226">
+<edge COLOR="#7c007c"/>
+</node>
+</node>
+</map>
