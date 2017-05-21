e61939ca108fad47c8c4cf426c69ff98d7fda64f
Universum Spiritus <universum.spiritus@gmail.com>
2017-05-22 07:01:16 +1000

    Renamed installing_r file to 'Installing R on Ubuntu.txt' and updated the Next is what.mm with notes on emergence as genesis of neural networks.

--- notes/Next is What/Next is what.mm
+++ notes/Next is What/Next is what.mm
@@ -1,8 +1,8 @@
 <map version="freeplane 1.5.9">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Next is what" FOLDED="false" ID="ID_500308797" CREATED="1494370453478" MODIFIED="1494370477552" STYLE="oval">
-<font SIZE="14"/>
-<hook NAME="MapStyle" zoom="1.464">
+<node TEXT="Next is what" FOLDED="false" ID="ID_500308797" CREATED="1494370453478" MODIFIED="1495189133445" STYLE="oval">
+<font SIZE="12"/>
+<hook NAME="MapStyle" zoom="0.909">
     <properties fit_to_viewport="false;"/>
 
 <map_styles>
@@ -81,11 +81,48 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="1" RULE="ON_BRANCH_CREATION"/>
+<hook NAME="AutomaticEdgeColor" COUNTER="4" RULE="ON_BRANCH_CREATION"/>
 <node TEXT="Retail In-store shopping bot" POSITION="right" ID="ID_662982511" CREATED="1494370494054" MODIFIED="1494370581319">
 <edge COLOR="#ff0000"/>
 <node TEXT="references" ID="ID_1439749954" CREATED="1494370519902" MODIFIED="1494370522846">
 <node TEXT="Zensar paper" ID="ID_1751431958" CREATED="1494370530470" MODIFIED="1494370552610" LINK="http:/www.zensar.com/sites/default/files/temp/case-study/Designing_an_Effective_Instore_Shopping_Assistant_Using_Bots.pdf"/>
+</node>
+</node>
+<node TEXT="philosophical roots&#xa;of data science" POSITION="left" ID="ID_1890765799" CREATED="1495189238993" MODIFIED="1495232337425">
+<edge COLOR="#0000ff"/>
+<node TEXT="neural network" ID="ID_1813506719" CREATED="1495189351677" MODIFIED="1495189358082">
+<node TEXT="emergent systems" ID="ID_741584166" CREATED="1495189359059" MODIFIED="1495225195597"/>
+</node>
+<node TEXT="ontology" ID="ID_283511467" CREATED="1495225577160" MODIFIED="1495225579957">
+<node TEXT="study of nature of existence" ID="ID_1554270072" CREATED="1495225581735" MODIFIED="1495225596827"/>
+<node TEXT="ontology engineering" ID="ID_1892197584" CREATED="1495225639303" MODIFIED="1495225651545">
+<node TEXT="semantic decision tables" ID="ID_1123713431" CREATED="1495225923451" MODIFIED="1495225929738"/>
+<node TEXT="knowledge representation language" ID="ID_1006678988" CREATED="1495225984410" MODIFIED="1495226011338"/>
+</node>
+</node>
+<node TEXT="epistemology" ID="ID_335404196" CREATED="1495226038538" MODIFIED="1495226043004">
+<node TEXT="theory of knowledge" ID="ID_90907250" CREATED="1495226588729" MODIFIED="1495226593770">
+<node TEXT="nature of knowledge" ID="ID_1773538309" CREATED="1495226610550" MODIFIED="1495226615454"/>
+<node TEXT="justification" ID="ID_1086317375" CREATED="1495226618099" MODIFIED="1495226621112"/>
+<node TEXT="rationality of belief" ID="ID_712744389" CREATED="1495226623667" MODIFIED="1495226639230"/>
+</node>
+<node TEXT="knowledge" ID="ID_1507814089" CREATED="1495227049685" MODIFIED="1495227053811">
+<node TEXT="knowing a concept" ID="ID_1797915055" CREATED="1495227017910" MODIFIED="1495227036953">
+<node TEXT="knowing that" ID="ID_25108111" CREATED="1495227144923" MODIFIED="1495227671237"/>
+</node>
+<node TEXT="understanding an operation" ID="ID_404296521" CREATED="1495227037552" MODIFIED="1495227044231">
+<node TEXT="knowing how" ID="ID_969502611" CREATED="1495227673856" MODIFIED="1495227676187"/>
+</node>
+<node TEXT="acquaintance knowledge" ID="ID_960023014" CREATED="1495227072643" MODIFIED="1495227110575"/>
+<node TEXT="belief" ID="ID_1301243655" CREATED="1495229209887" MODIFIED="1495229214016">
+<node TEXT="what I accept as truth from my cognitive point of view" ID="ID_1065806183" CREATED="1495229224952" MODIFIED="1495229260192"/>
+</node>
+<node TEXT="truth" ID="ID_800511948" CREATED="1495230797028" MODIFIED="1495230800124">
+<node TEXT="something is actually known" ID="ID_173516156" CREATED="1495230800905" MODIFIED="1495230809716"/>
+</node>
+<node TEXT="Gettier counterexample" ID="ID_840349940" CREATED="1495232286258" MODIFIED="1495232303530"/>
+</node>
+<node TEXT="accquiring knowledge" ID="ID_619455618" CREATED="1495232318079" MODIFIED="1495232328050"/>
 </node>
 </node>
 </node>
