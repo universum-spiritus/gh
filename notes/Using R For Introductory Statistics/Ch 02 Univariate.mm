c3eaad913d9c3dc6a3ee44bd66c115c8e69cbd68
SYGMM <milind@sumyag.com>
2017-05-29 21:27:41 +1000

    Added mind maps for univariate and getting-started

---
+++ notes/Using R For Introductory Statistics/Ch 02 Univariate.mm
@@ -0,0 +1,127 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Univariate" FOLDED="false" ID="ID_996033394" CREATED="1496036157820" MODIFIED="1496036185548" STYLE="oval">
+<font SIZE="10"/>
+<hook NAME="MapStyle" zoom="1.331">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="10" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="data set" POSITION="right" ID="ID_580820697" CREATED="1496042823028" MODIFIED="1496042825485">
+<edge COLOR="#00ff00"/>
+<node TEXT="case" ID="ID_1882951692" CREATED="1496042837265" MODIFIED="1496042838518">
+<node TEXT="an entitiy" ID="ID_162506228" CREATED="1496042878507" MODIFIED="1496042882047"/>
+</node>
+<node TEXT="variable" ID="ID_682289271" CREATED="1496042850823" MODIFIED="1496042852922">
+<node TEXT="characteristics of an entitiy" ID="ID_1159992441" CREATED="1496042860803" MODIFIED="1496042875574"/>
+</node>
+</node>
+<node TEXT="univariate data set" POSITION="right" ID="ID_1944366020" CREATED="1496043036412" MODIFIED="1496043043930">
+<edge COLOR="#ff00ff"/>
+<node TEXT="single variable data sets" ID="ID_1969246857" CREATED="1496036271395" MODIFIED="1496043055815"/>
+<node TEXT="summaries of univariate data" ID="ID_393753621" CREATED="1496036292911" MODIFIED="1496043055815"/>
+<node TEXT="represented as x1, x2, x3...xN" ID="ID_626191414" CREATED="1496043065413" MODIFIED="1496043082999">
+<node TEXT="subscripts 1, 2, ...N match&#xa;the value to corresponding&#xa;case" ID="ID_967542834" CREATED="1496043102865" MODIFIED="1496043131102"/>
+<node TEXT="measurements of same type" ID="ID_1392560470" CREATED="1496043804760" MODIFIED="1496048174155">
+<font BOLD="true"/>
+</node>
+<node TEXT="column matrix" ID="ID_1076159633" CREATED="1496043811505" MODIFIED="1496043816944"/>
+<node TEXT="represented by R&apos;s vector" ID="ID_1028933612" CREATED="1496043823977" MODIFIED="1496043836010"/>
+</node>
+</node>
+<node TEXT="levels of&#xa;measurement" POSITION="right" ID="ID_1045916812" CREATED="1496043318014" MODIFIED="1496043326516">
+<edge COLOR="#00ffff"/>
+<node TEXT="factor data" ID="ID_429915156" CREATED="1496043419264" MODIFIED="1496043425366">
+<node TEXT="nominal or categorical" ID="ID_291714291" CREATED="1496043366675" MODIFIED="1496043374429"/>
+<node TEXT="ordinal" ID="ID_1585461766" CREATED="1496043377830" MODIFIED="1496043379526"/>
+</node>
+<node TEXT="interval" ID="ID_1201867064" CREATED="1496043385601" MODIFIED="1496043388386">
+<node TEXT="no true zero" ID="ID_807162488" CREATED="1496043457515" MODIFIED="1496043461101"/>
+</node>
+<node TEXT="ratio" ID="ID_1336638939" CREATED="1496043390172" MODIFIED="1496043393113"/>
+</node>
+<node TEXT="granularity?" POSITION="right" ID="ID_1570309026" CREATED="1496043530960" MODIFIED="1496043538696">
+<edge COLOR="#007c00"/>
+<node TEXT="discrete data" ID="ID_900961205" CREATED="1496043540034" MODIFIED="1496043543414"/>
+<node TEXT="continuos data" ID="ID_1076114702" CREATED="1496043543752" MODIFIED="1496043551900"/>
+</node>
+<node TEXT="handling missing data" POSITION="left" ID="ID_1445132087" CREATED="1496048207036" MODIFIED="1496050003662">
+<edge COLOR="#7c007c"/>
+</node>
+</node>
+</map>
---
+++ notes/Using R For Introductory Statistics/Ch01 Getting Started.mm
@@ -0,0 +1,108 @@
+<map version="freeplane 1.5.9">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Getting&#xa;Started" FOLDED="false" ID="ID_1655994897" CREATED="1496034221749" MODIFIED="1496035497544" STYLE="oval">
+<font SIZE="10"/>
+<hook NAME="MapStyle" zoom="1.331">
+    <properties fit_to_viewport="false;"/>
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node" STYLE="oval" UNIFORM_SHAPE="true" VGAP_QUANTITY="24.0 pt">
+<font SIZE="24"/>
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="default" COLOR="#000000" STYLE="fork">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.attributes">
+<font SIZE="9"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.note" COLOR="#000000" BACKGROUND_COLOR="#ffffff" TEXT_ALIGN="LEFT"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="bubble">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="oval" SHAPE_HORIZONTAL_MARGIN="10.0 pt" SHAPE_VERTICAL_MARGIN="10.0 pt">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+<edge COLOR="#ff0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+<edge COLOR="#0000ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+<edge COLOR="#00ff00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+<edge COLOR="#ff00ff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,5">
+<edge COLOR="#00ffff"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,6">
+<edge COLOR="#7c0000"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,7">
+<edge COLOR="#00007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,8">
+<edge COLOR="#007c00"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,9">
+<edge COLOR="#7c007c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,10">
+<edge COLOR="#007c7c"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,11">
+<edge COLOR="#7c7c00"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="3" RULE="ON_BRANCH_CREATION"/>
+<node TEXT="objective and emergent&#xa;relationships may exist&#xa;between variables" POSITION="right" ID="ID_627852491" CREATED="1496034290149" MODIFIED="1496034711957">
+<edge COLOR="#ff0000"/>
+<node TEXT="it may not be cause and effect" ID="ID_109151167" CREATED="1496034373433" MODIFIED="1496034384198"/>
+<node TEXT="large good quality data sets&#xa;are required for discovering&#xa; these relationships" ID="ID_214643172" CREATED="1496034526252" MODIFIED="1496034622766"/>
+<node TEXT="the context of data is important&#xa;too for making decisions" ID="ID_1835098400" CREATED="1496034730056" MODIFIED="1496034751640"/>
+<node TEXT="understanding the factors that&#xa;contribute to the statistic" ID="ID_1065142043" CREATED="1496035289519" MODIFIED="1496035305571"/>
+</node>
+<node TEXT="statistical inference" POSITION="right" ID="ID_1318139577" CREATED="1496035508361" MODIFIED="1496035516396">
+<edge COLOR="#0000ff"/>
+<node TEXT="draw conclusions about populations" ID="ID_547603882" CREATED="1496035641211" MODIFIED="1496035650959"/>
+<node TEXT="from samples collected  from&#xa;these populations" ID="ID_1416030327" CREATED="1496035678847" MODIFIED="1496035701443"/>
+<node TEXT="using the language of&#xa;probability" ID="ID_43213726" CREATED="1496035772500" MODIFIED="1496035782334">
+<node TEXT="describe concrete things using data&#xa;subject to random variation" ID="ID_1542471380" CREATED="1496035804707" MODIFIED="1496035961745"/>
+<node TEXT="require us to make models of data" ID="ID_836047239" CREATED="1496035978559" MODIFIED="1496035985553"/>
+</node>
+</node>
+<node TEXT="organization of the&#xa;text book" POSITION="right" ID="ID_786846670" CREATED="1496036025525" MODIFIED="1496036067740">
+<edge COLOR="#00ff00"/>
+<node TEXT="exploring data" ID="ID_1275137101" CREATED="1496036076562" MODIFIED="1496036081517"/>
+<node TEXT="statistical inference" ID="ID_334538903" CREATED="1496036098614" MODIFIED="1496036105363"/>
+<node TEXT="modeling of data" ID="ID_107083123" CREATED="1496036111800" MODIFIED="1496036115018"/>
+</node>
+</node>
+</map>
